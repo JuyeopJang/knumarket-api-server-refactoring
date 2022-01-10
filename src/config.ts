9e6beb2b2ff18032cddafa50f31121aaa3e4dc5a
juyeop <shininggiver@gmail.com>
2022-01-10 10:52:17 +0900

    Fix connection file fixed 20220110 10:52

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,9 @@ dotenv.config();
 export const jwtSecret = process.env.JWT_SECRET;
 export const node_env = process.env.NODE_ENV;
 
-export const database = process.env.DB;
+// db info
+export const db = process.env.DB;
+export const db_host = process.env.DB_HOST;
+export const db_port = process.env.DB_PORT;
+export const db_user = process.env.DB_USER;
 export const db_password = process.env.DB_PASSWORD;
