d9abc0eb01791e3fc0da4995f0d6215c3448085e
juyeop <shininggiver@gmail.com>
2022-01-10 10:08:55 +0900

    Fix 최신 커밋 이력 삭제 20210110 10:08

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,6 @@ dotenv.config();
 
 export const jwtSecret = process.env.JWT_SECRET;
 export const node_env = process.env.NODE_ENV;
+
+export const database = process.env.DB;
+export const db_password = process.env.DB_PASSWORD;
