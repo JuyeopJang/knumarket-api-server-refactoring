9f114b654ae4b921d8fa1a49c2dacf2ea3def3c6
juyeop <shininggiver@gmail.com>
2022-03-06 17:44:29 +0900

    update updated app.ts 20220306 17:44

--- src/app.ts
+++ src/app.ts
@@ -27,8 +27,8 @@ class App {
             next();
         })
         this.app.use(bodyParser.json());
-        this.app.use(helmet());
-        // this.app.use(morgan("combined"));
+        this.app.use(helmet())
+        this.app.use(morgan("combined"));
     }
 
     initializeErrorHandling() {
